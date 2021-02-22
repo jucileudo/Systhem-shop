1f17285b664f80e4bb41f5c055b0ce8eabd92ddc
Jucileudo <juci0310@hotmail.com>
2021-02-22 19:31:09 -0300

    Adicionar arquivos de projeto.

---
+++ src/Systhem.Auth.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Systhem.Auth.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/Systhem.Auth.Api/Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Systhem.Auth.Api.Data
+{
+    public class ApplicationContext : IdentityDbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
